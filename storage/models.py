f8e62e22436f2cd2304ffa7c761d70ca19815bee
Oduol Peris <oduolpepe618@gmail.com>
2024-03-24 04:13:04 +0300

    Add a Customer and a Ordr Object

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200 null=true)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length = 200)
+    price = models.FloatFied()
+    digital = models.BooleanField(default=False, null=true, blank=True)
+
+    def __str__(self):
+        return self.name
