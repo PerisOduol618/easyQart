2a1c628de814ec6ef42f305873d2ae2580fd6596
Oduol Peris <oduolpepe618@gmail.com>
2024-03-24 05:19:39 +0300

    migrate the database

--- storage/models.py
+++ storage/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200, null=true)
+    email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
@@ -14,7 +14,7 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length = 200)
     price = models.FloatField()
-    digital = models.BooleanField(default=False, null=true, blank=True)
+    digital = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +23,7 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)  #many to one relationship
     date_ordered = models.DateTimeField(auto_now_add= True)
     complete = models.BooleanField(default=False) #changes the status of the cart
-    transaction_id = models.CharField(max_length=100, null=true)
+    transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return str(self.id)
@@ -43,9 +43,9 @@ class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)  #many to one relationship
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     address = models.CharField(max_length=200, null=False)
-    city = models.CharField(max_length==200, null=False)
-    state = models.CharField(max_length==200, null=False)
-    zipcode = models.CharField(max_length==200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
 
     def __str__(self):
         return self.address
