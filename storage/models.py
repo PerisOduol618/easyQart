23b4f4e3849c0b75daf03b39e1c009438a9047dc
Oduol Peris <oduolpepe618@gmail.com>
2024-03-24 05:01:19 +0300

    add a OrderItems model

--- storage/models.py
+++ storage/models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.CharField(max_length=200 null=true)
+    email = models.CharField(max_length=200, null=true)
 
     def __str__(self):
         return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length = 200)
-    price = models.FloatFied()
+    price = models.FloatField()
     digital = models.BooleanField(default=False, null=true, blank=True)
 
     def __str__(self):
@@ -27,3 +27,12 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+'''
+Items that need to be added to our order with a many to one relationship with our order
+'''
+class OrderItems(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    quantity = models.IntegerField(default=0, null= True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
