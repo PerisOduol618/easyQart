b967533a23823d3e7657069603c421147ff6b124
Oduol Peris <oduolpepe618@gmail.com>
2024-03-24 05:13:47 +0300

    add shipping adress model

--- storage/models.py
+++ storage/models.py
@@ -28,11 +28,26 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
-'''
-Items that need to be added to our order with a many to one relationship with our order
-'''
+
+#Items that need to be added to our order with a many to one relationship with our order
+
 class OrderItems(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null= True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+# This model will be a child to order and will only be created if at the ordertime within an order is physical product(if product.digital=False)
+ 
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)  #many to one relationship
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length==200, null=False)
+    state = models.CharField(max_length==200, null=False)
+    zipcode = models.CharField(max_length==200, null=False)
+
+    def __str__(self):
+        return self.address
+
+
