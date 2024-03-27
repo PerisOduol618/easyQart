824a0079ca532e1ed323fa4c1515a1c7f951e431
Oduol Peris <oduolpepe618@gmail.com>
2024-03-27 08:22:51 +0300

    render cart items in the database  to the cart temlaplate

--- storage/views.py
+++ storage/views.py
@@ -8,7 +8,14 @@ def store(request):
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    # set conditions fro an authenticated user and one for a user the is not logged in
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitems_set.all()
+    else:
+        items = []
+    context = {'items':items}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
