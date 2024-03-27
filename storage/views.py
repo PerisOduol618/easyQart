eedf1323c81aa47292f7d208bdbf589941440896
Oduol Peris <oduolpepe618@gmail.com>
2024-03-27 14:21:20 +0300

    add update item view

--- storage/views.py
+++ storage/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 from .models import *
 
 # Create your views here.
@@ -30,3 +32,5 @@ def checkout(request):
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
 
+def updateItem(request):
+    return JsonResponse('item was added', safe=False)
