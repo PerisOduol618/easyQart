e9c1f859264607cb06d917851a36cbfa3e267da0
Oduol Peris <oduolpepe618@gmail.com>
2024-03-27 13:33:53 +0300

    Add an event handler in a loop to query the buttons by the class of update-cart

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,9 @@
-console.log('Hello DJango')
+const updateButton = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateButton.length; i++){
+    updateButton[i].addEventListener('click', function(){
+        let productId = this.dataset.product
+        let action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
+    })
+}
