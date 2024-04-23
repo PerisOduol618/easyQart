c7a9cb3475466e0383bb71b23fe62b6e2a2329ee
Oduol Peris <oduolpepe618@gmail.com>
2024-04-23 05:22:39 +0300

    🎨: Add app account to the project setting and set the urls

--- easyQart/urls.py
+++ easyQart/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('storage.urls')),
-    path('auth', include('auth.urls'))
+    path('account/', include('account.urls'))
 
 ]
 
